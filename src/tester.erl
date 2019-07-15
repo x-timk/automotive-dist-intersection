f241452aa3e0017bfc8f68d0cb816bb039a1ed6a
x-timk <x-timk>
2019-07-15 19:01:08 +0200

    removed test stuff

--- src/tester.erl
+++ src/tester.erl
@@ -3,12 +3,6 @@
 -export([main/0]).
 
 
-aux_next_pos([_ | [S | _]] ) ->
-  {Node,_} = S,
-  Node;
-aux_next_pos(_) ->
-  arrived.
-
 main() ->
   % dim_env:tester().
   % List = [{inord,tail},{isud,asd},{c,addd}],
